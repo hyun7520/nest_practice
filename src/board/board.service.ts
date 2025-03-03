ca12308711c52279df2d5cfa9bdcde4434bc0c28
hyun7520 <sanghyun123452@gmail.com>
2025-03-03 21:35:10 +0900

    ADD: 게시판 create

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardService {
@@ -9,5 +10,21 @@ export class BoardService {
     // 모든 게시판 조회: 타입을 똑같이 정의
     getAllBoards(): Board[] {
         return this.board;
+    }
+
+    createBoard(title: string, decription: string): Board {
+        const board: Board = {
+            // title: title,
+            // decription: decription,
+            // 명칭이 동일할 경우 생략가능
+            id: uuid(),
+            title,
+            decription,
+            status: BoardStatus.PUBLIC,
+        }
+
+        // 새로운 게시물을 입력
+        this.board.push(board);
+        return board;
     }
 }
