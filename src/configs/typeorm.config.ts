8bc461d85995902f63ba7be45f4839257447f509
hyun7520 <sanghyun123452@gmail.com>
2025-03-14 21:18:04 +0900

    FIX: env 임시 설정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -5,7 +5,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
     host: 'localhost',
     port: 5432,
     username: 'postgres',
-    password: '7520',
+    password: '1234',
     database: 'Board',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true
