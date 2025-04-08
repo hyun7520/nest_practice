4794a241a1f211d6870546a6508ed0ce58474d20
hyun7520 <sanghyun123452@gmail.com>
2025-04-08 19:56:23 +0900

    ADD: JWT 기능

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,22 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt'
+    }),
+    // yarn add @nestjs/jwt @nestjs/passport passport passport-jwt --save
+    // Jwt를 사용하기 위해 명령어로 모듈 설치 먼저
+    JwtModule.register({
+      secret: 'secret1234',
+      signOptions: {
+        expiresIn: 3600
+      }
+    }),
     TypeOrmModule.forFeature([UserRepository]),
   ],
   controllers: [AuthController],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,25 +4,31 @@ import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credentials.dto';
 import { UserEntity } from './user.entity';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
         private userRepository: UserRepository,
+        private jwtService: JwtService,
     ) { }
 
     async signUp(authCredentialDto: AuthCredentialDto): Promise<UserEntity> {
         return this.userRepository.createUser(authCredentialDto);
     }
 
-    async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+    async signIn(authCredentialDto: AuthCredentialDto): Promise<{ accessToken: string }> {
         const { username, password } = authCredentialDto;
         const user = await this.userRepository.findOne({
             where: { username: username }
         });
         if (user && (await bcrypt.compare(password, user.password))) {
-            return 'login success';
+            // 사용자 토큰 생성 secret + payload
+            const payload = { username };
+            const accessToken = await this.jwtService.sign(payload);
+
+            return { accessToken: };
         }
         else {
             throw new UnauthorizedException('login failed')
